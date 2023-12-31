5c279576e301bc3f5b1b0d7bedbe7488d56f40eb
Glitched Panda <82221876+GlitchedPanda@users.noreply.github.com>
2023-12-31 11:19:51 +0100

    Create 1-HelloWorld.ino

---
+++ Programs/1-HelloWorld/1-HelloWorld.ino
@@ -0,0 +1,57 @@
+/*******************************************************************
+    Hello World for the ESP32 Cheap Yellow Display.
+
+    https://github.com/witnessmenow/ESP32-Cheap-Yellow-Display
+
+    If you find what I do useful and would like to support me,
+    please consider becoming a sponsor on Github
+    https://github.com/sponsors/witnessmenow/
+
+    Written by Brian Lough
+    YouTube: https://www.youtube.com/brianlough
+    Twitter: https://twitter.com/witnessmenow
+ *******************************************************************/
+
+// Make sure to copy the UserSetup.h file into the library as
+// per the Github Instructions. The pins are defined in there.
+
+// Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
+// Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
+// Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+// Note the following larger fonts are primarily numeric only!
+// Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+// Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
+// Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+
+#include <TFT_eSPI.h>
+// A library for interfacing with LCD displays
+//
+// Can be installed from the library manager (Search for "TFT_eSPI")
+//https://github.com/Bodmer/TFT_eSPI
+
+TFT_eSPI tft = TFT_eSPI();
+
+void setup() {
+  // Start the tft display and set it to black
+  tft.init();
+  tft.setRotation(1); //This is the display in landscape
+  
+  // Clear the screen before writing to it
+  tft.fillScreen(TFT_BLACK);
+
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  int x = 5;
+  int y = 10;
+  int fontNum = 2; 
+  tft.drawString("Hello", x, y, fontNum); // Left Aligned
+  x = 320 /2;
+  y += 16;
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);
+  tft.drawCentreString("World", x, y, fontNum);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
